a2e8a49bc4b23a627b982e655d70bdbb149a516f
Aubrey Graves <grav0165@gmail.com>
2023-06-12 20:43:29 -0500

    created arcane scepter function and functiont to check the status of player AP and fungus HP bar

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(onReady);
 // Feel free to make this to what you want!
 // Example:
 // let fungusHP = 100;
-let fongusHP = 100;
+let fungusHp = 100;
 let apBar = 100;
 
 function onReady() {
@@ -23,5 +23,50 @@ function acAttack() {
     console.log('arcane scepter attack');
     // - **AP Cost:** 12
     // - **HP Damage:** 14
-    
+
+    if(apBar > 12) {
+        apBar -= 12;
+        fungusHp -=14;
+        console.log('New AP bar value', apBar);
+        console.log('New Fungus HP is: ', fungusHp);
+        $('.hp-text').text(fungusHp);
+        $('#hp-meter').val(fungusHp);
+        $('.ap-text').text(apBar);
+        $('#ap-meter').val(apBar);
+    } else {
+        
+    }
+    checkStatus();
+}
+
+function acAttack() {
+    console.log('entangle');
+    // - **AP Cost:** 23
+    // - **HP Damage:** 9
+
+    if(apBar > 12) {
+        apBar -= 12;
+        fungusHp -=14;
+        console.log('New AP bar value', apBar);
+        console.log('New Fungus HP is: ', fungusHp);
+        $('.hp-text').text(fungusHp);
+        $('#hp-meter').val(fungusHp);
+        $('.ap-text').text(apBar);
+        $('#ap-meter').val(apBar);
+    } else {
+        
+    }
+    checkStatus();
+}
+
+function checkStatus() {
+    if(apBar <= 0) {
+        $('.freaky-fungus').removeClass('.walk');
+        $('.freaky-fungus').addClass('jump');
+    }
+    if(fungusHp <= 0) {
+        $('.freaky-fungus').removeClass('.walk');
+        $('.freaky-fungus').addClass('dead');
+    }
+
 }
